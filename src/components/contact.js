ce06c9a60273d7a7078a06c1d5e76157e33442f7
manisankar25 <sankarkantipudi@gmail.com>
2022-06-07 19:46:58 +0530

    contact details added

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { AiFillControl } from 'react-icons/fa';
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+
 
 const Contact = () => {
     return (
@@ -9,10 +12,8 @@ const Contact = () => {
                     <div id="contact-bar" class="bar"></div>
                     <div className="header-text">I'm currently open to new career opportunities. Would you like to contact me? Feel free to leave me a message, or reach out through the websites below:</div>
                     <div id="social-media">
-                        <a id="linkedin-link" class="contact-button" href="https://nl.linkedin.com/in/vincentoldenburg" target="_blank" title="Linkedin.com/in/VincentOldenburg"><i class="fa-brands fa-linkedin"></i></a>
-                        <a id="github-link" class="contact-button" href="https://www.github.com/trajectum" target="_blank" title="Github.com/Trajectum"><FontAwesomeIcon icon="fa-brands fa-github" /></a>
-                        <a id="codepen-link" class="contact-button" href="https://www.codepen.io/dirack" target="_blank" title="Codepen.io/Dirack"><i class="fa fa-5x fa-codepen"></i></a>
-                        <a id="profile-link" class="contact-button" href="https://www.freecodecamp.org/dirack" target="_blank" title="FreeCodeCamp.com/Dirack"><i class="fa fa-5x fa-free-code-camp"></i></a>
+                        <a id="linkedin-link" class="contact-button" href="https://www.linkedin.com/in/mani-sankar-72985093/" target="_blank" title="Linkedin.com/in/ManiSankar"><AiFillLinkedin /></a>
+                        <a id="github-link" class="contact-button" href="https://github.com/manisankar25" taret="_blank" title="Github.com/Trajectum"><AiFillGithub /></a>
                     </div>
                 </div>
                 <div>
